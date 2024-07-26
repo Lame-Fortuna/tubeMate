421f2be19a3dad0e3bf8019ceddfa8fe21fc0dac
Lame-Fortuna <pi.patel.2207@gmail.com>
2024-07-27 02:30:55 +0530

    Delete routes directory

--- routes/videoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const videoController = require('../controllers/visible');
-
-router.get('/video/', videoController.renderVideoPage);
-router.get('/video/:vidId', videoController.renderVideoPageWithVidId);
-router.post('/video/convert', videoController.convertVideo);
-
-module.exports = router;
