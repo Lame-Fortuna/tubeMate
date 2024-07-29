d6063df2b25d4a8c10fa5de308d2309058733640
Lame-Fortuna <pi.patel.2207@gmail.com>
2024-07-29 20:34:41 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const fs = require('fs')
 const router = express.Router();
 
 // Home Route
 router.get('/', (req, res) => {
+    const remove = () => {
+        fs.rm('files', { recursive: true }, (error) => {
+            if (error) {
+                console.log('Error deleting contents of files because: ', error);
+            } else {
+                console.log('Cleared the contents of files');
+            }
+        });
+    };
+    
+    remove();
+
     res.render("yt");
 });
 
